943de2ce3eb154ea222991a896d5b69482d96575
h3nrym4nX <hhpaucara.2110@outlook.com>
2022-06-14 11:06:06 -0500

    function tienenMismaLongitud

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,46 +28,68 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+    return str;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  var x= 1;
+  var y= 2;
+  var suma = x+y;
+  return suma;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  var x= 1;
+  var y= 2;
+  var resta = y-x;
+  return resta;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  var x= 1;
+  var y= 2;
+  var producto = x*y;
+  return producto;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  var x= 4;
+  var y= 2;
+  var division = x/y;
+  return division;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  var x= 4;
+  var y= 2;
+ if (x === y) {
+  return true;
+ }
+   return false;
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  var str1 = "luis";
+  var str2 = "juana";
+ if (str1.length === str2.length) {
+  return true;
+}
+return false;
 }
 
 function menosQueNoventa(num) {
